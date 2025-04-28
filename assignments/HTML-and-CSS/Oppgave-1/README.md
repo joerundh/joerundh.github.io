dd0d4a6fbb4a4ece628e1ef48bb3007cbc23515c
joerundh <joerundh.p01@gmail.com>
2025-04-28 14:07:38 +0200

    removed copies, links to repos

--- assignments/HTML-and-CSS/Oppgave-1/README.md
+++
@@ -1,3 +0,0 @@
-# Hashtag Readme
-
-Note: image credits I cannot account for at the present time, they were all found by Google searching.
--- assignments/Javascript-Advanced/Oppgave-2/js/nav.js
+++
@@ -1,9 +0,0 @@
-const sections = [
-    document.getElementById("lists"),
-    document.getElementById("add-book")
-];
-
-function goToSection(index) {
-    sections.forEach(section => section.style.display = "none");
-    sections[index].style.display = "inline-block";
-}
